e62494f0bd3c8edefec8fc595d5bb8c030fe972f
tahtah1995 <34459113+tahtah1995@users.noreply.github.com>
2021-04-13 20:21:54 +0200

    Update jwt.js

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -9,12 +9,14 @@ function authJwt() {
         isRevoked: isRevoked
     }).unless({
         path: [
+             /\/public\/uploads(.*)/,
+             `${api}/users/login`,
+            `${api}/users/register`,
             {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
-            `${api}/users/login`,
-            `${api}/users/register`,
+           
         ]
     })
 }
