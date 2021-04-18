dc63d2cc560a57932775f70585462db9281f213b
tahtah1995 <34459113+tahtah1995@users.noreply.github.com>
2021-04-18 16:41:36 +0200

    Update jwt.js

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -16,6 +16,7 @@ function authJwt() {
             {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
+            { url: /\/api\/v1\/users\/.*/,methods: ['GET']},
            
         ]
     })
